687699d30242b41bca3cbf38c49bb5fbed33d795
Eduardo Função <45571086+eduardofuncao@users.noreply.github.com>
2024-05-28 01:23:08 -0300

    adiciona navbar e footer, melhora disposição de elementos

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,20 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center">
-      <div className="text-9xl mb-12 mt-24">Oceanômetro</div>
-      <div className="h-32 w-[30rem] mb-4 bg-black rounded-3xl"></div>
-      <p className="text-2xl">A cada um real doado, você...</p>
-      <button className="bg-red-500 rounded px-4"> Doe aqui</button>
+    <main className="w-[90vw] m-auto">
+      <nav className="flex justify-between">
+        <div>LOGO</div>
+        <div>CONTATO</div>
+      </nav>
+      <div className="flex flex-col items-center justify-center">
+        <div className="text-9xl mb-12 mt-24">Oceanômetro</div>
+        <div className="h-32 w-[30rem] mb-4 bg-black rounded-3xl"></div>
+        <p className="text-2xl">A cada um real doado, você...</p>
+        <button className="bg-red-500 rounded px-4"> Doe aqui</button>
+      </div>
+      <footer className="fixed bottom-0 left-[45vw]">
+        <p>CR AEJ 2024</p>
+      </footer>
     </main>
   );
 }

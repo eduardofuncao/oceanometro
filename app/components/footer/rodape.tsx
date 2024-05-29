66e25db87660923e624daff54db8b2d616833a36
ArtFiorindo <alopesfiorindo@gmail.com>
2024-05-29 20:03:29 -0300

    Ajustando Footer

--- app/components/footer/rodape.tsx
+++ app/components/footer/rodape.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
     return (
       <footer className="bg-gray-800 text-gray-300 py-4 text-center">
         <div className="container mx-auto">
-          <p>&copy; 2024 Oceanometro. Todos os direitos reservados à Artur Fiorindo | Eduardo Função | Jhoe Hashimoto.</p>
+          <p>&copy; 2024 Oceanometro. Todos os direitos reservados à Artur Fiorindo rm553481 | Eduardo Função rm553362 | Jhoe Hashimoto rm553831. 1TDSZ</p>
           <br></br>
           <p>Lonk do GitHub: <a href="https://github.com/eduardofuncao/oceanometro" target="blank" className="underline text-blue-600">https://github.com/eduardofuncao/oceanometro</a></p>
         </div>

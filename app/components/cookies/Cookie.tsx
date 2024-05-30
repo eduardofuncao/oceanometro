06c4c9fef9f6f54cbfd4f504fff086587df3c705
ArtFiorindo <alopesfiorindo@gmail.com>
2024-05-30 18:01:09 -0300

    Ajustando botões aceite de cookies

--- app/components/cookies/Cookie.tsx
+++ app/components/cookies/Cookie.tsx
@@ -22,13 +22,15 @@ const CookiePopup = () => {
 
     return (
         <div 
-            className={`fixed bottom-4 left-4 right-4 bg-slate-300 p-4 rounded-md shadow-md z-50 ${show ? 'block' : 'hidden'}`}
-            style={{ zIndex: 50 }} // Adicionado z-index
+            className={`fixed bottom-4 left-4 right-4 bg-slate-300 p-6 rounded-md shadow-md z-50 ${show ? 'block' : 'hidden'}`}
+            style={{ zIndex: 50, display: 'flex', alignItems: 'center', justifyContent: 'space-between' }} // Adicionado display flex e ajustado o alinhamento
         >
-            <p className="text-gray-800">Nosso site utiliza cookies para oferecer a melhor experiência. Ao continuar, você concorda com nossa <em>Política de Privacidade.</em></p>
+            <p className="text-gray-800 flex-grow">Nosso site utiliza cookies para oferecer a melhor experiência. Ao continuar, você concorda com nossa <em>Política de Privacidade.</em></p>
             
-            <button className="bg-slate-600 hover:bg-blue-900 text-white px-4 py-2 rounded mr-2" onClick={acceptCookies}><u>Aceitar</u></button>
-            <button className="bg-slate-400 hover:bg-red-900 text-white px-4 py-2 rounded" onClick={declineCookies}>Recusar</button>
+            <div>
+                <button className="bg-slate-600 hover:bg-blue-900 text-white px-4 py-2 rounded mr-2" onClick={acceptCookies}><u>Aceitar</u></button>
+                <button className="bg-slate-400 hover:bg-red-900 text-white px-4 py-2 rounded" onClick={declineCookies}>Recusar</button>
+            </div>
         </div>
     );
 };
